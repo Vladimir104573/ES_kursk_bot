1616fd3821ee31bd1c9a3380c5af5f0b643f7515
Vladimir104573 <134796954+Vladimir104573@users.noreply.github.com>
2024-12-24 01:38:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import logging
+import time
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.methods import DeleteWebhook
+
+
+bot = Bot(token="7443716696:AAGy_Ze48ZO49sDHPR-SebHiCHLic7vtNsU")
+
+# Ссылка на бота --> https://t.me/ES_Kursk_Bot
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message):
+
+    await message.answer(f"Здравствуйте, {message.from_user.full_name}!")
+
+
+@dp.message()
+async def message_handler(message: Message):
+
+    update_time = time.strftime('%d.%m.%Y %H+3:%M:%S', time.gmtime())
+    logging.info(f"{update_time}; chat id = {message.from_user.id}; user name = {message.from_user.full_name}; "
+                 f"message type = {message.content_type}")
+
+    await message.answer("Сообщение доставлено. Спасибо, что связались с нами!")
+
+
+async def main():
+    await bot(DeleteWebhook(drop_pending_updates=True))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
